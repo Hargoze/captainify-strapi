267ba0c7b94f6db648fc12eaf71a5c4fd3b86935
Hargoze <francois.lecarpentier99@gmail.com>
2021-02-01 11:36:13 +0100

    add all

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b8fcca742ea7c7ffa4380937babc0fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb13ee45-a845-4efa-a7d3-9227a6d79542'
+};
